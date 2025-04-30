a9f44b32950bbbe5c226acfc8637677993668488
manuel orlandi <manu.orlandi91@gmail.com>
2025-04-30 22:22:13 +0200

    updated system prompt

--- src/data/main.py
+++ src/data/main.py
@@ -1,11 +1,34 @@
 import os
+import streamlit as st
 import polars as pl
-from langchain.document_loaders import DataFrameLoader
+from langchain_community.document_loaders import DataFrameLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+# from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate
+)
+from langchain_openai import ChatOpenAI
 
 
-def load_dataset(dataset_name: str = "dataset.csv") -> pl.DataFrame:
+SYSTEM_PROMPT = """
+You are a chatbot tasked with responding to questions about the documentation of the LangChain library and project.
+
+You should never answer a question with a question, and you should always respond with the most relevant documentation page.
+
+Given a question, you should respond providing an exhaustive answer from information retrieved from the documentation and add a link with the most relevant documentation page for further information by following the relevant SYSTEM_PROMPT below:\n
+{context}
+"""
+
+
+def load_dataset(dataset_name: str = "langchain_docs.csv") -> pl.DataFrame:
     """
     Load the dataset from the csv file
 
@@ -15,7 +38,7 @@ def load_dataset(dataset_name: str = "dataset.csv") -> pl.DataFrame:
     Returns:
         pl.DataFrame: The loaded dataset containing data gathered by langchain
     """
-    data_dir = "./data"
+    data_dir = "./src/data"
     file_path = os.path.join(data_dir, dataset_name)
     df = pl.read_csv(file_path)
     return df
@@ -37,15 +60,20 @@ def create_chunks(
     Returns:
         list[pl.DataFrame]: The list of chunks
     """
-    text_chunks = DataFrameLoader(df, page_content_column="body").load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap,
         length_function=len,
     )
+    text_chunks = DataFrameLoader(
+        df.to_pandas(),
+        page_content_column="body"
+    ).load_and_split(
+        text_splitter=text_splitter
+    )
 
-    # add metadata to chucks to simplofy retrieval
-    for doc in text_splitter:
+    # add metadata to chucks to simplify retrieval
+    for doc in text_chunks:
         title = doc.metadata["title"]
         description = doc.metadata["description"]
         content = doc.page_content
@@ -67,7 +95,25 @@ def create_or_get_vector_store(chunks: pl.DataFrame) -> FAISS:
     Returns:
         FAISS: The vector store
     """
-    pass
+
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+
+    if not os.path.exists("./db"):
+        print("Creating vector store")
+        vector_store = FAISS.from_documents(chunks, embeddings)
+        vector_store.save_local("./db")
+    else:
+        print("Loading vector store")
+        vector_store = FAISS.load_local(
+            folder_path="./db",
+            embeddings=embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+    return vector_store
 
 
 def get_conversation_chain(
@@ -86,11 +132,74 @@ def get_conversation_chain(
     Returns:
         ConversationChain: The configured conversation chain for question answering
     """
-    pass
+
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory,
+        combine_docs_chain_kwargs={
+            "prompt": ChatPromptTemplate.from_messages(
+                [
+                    system_message,
+                    human_message,
+                ]
+            ),
+        },
+    )
+    return conversation_chain
 
 
 def main():
-    pass
+    load_dotenv()
+    df = load_dataset()
+    chunks = create_chunks(df, 1000, 0)
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        SYSTEM_PROMPT
+    )
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        "{question}"
+    )
+
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = create_or_get_vector_store(chunks)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation_chain = get_conversation_chain(
+            st.session_state.vector_store,
+            human_message_prompt,
+            system_message_prompt
+        )
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.set_page_config(
+        page_title="Documentation Chatbot",
+        page_icon=":books:",
+    )
+
+    st.title("Documentation Chatbot")
+    st.subheader("Documentation Chatbot")
+    st.markdown(
+        """
+        TEST CHATBOT
+        """
+    )
+    st.image("https://python.langchain.com/svg/langchain_stack_112024_dark.svg") 
+
+    user_question = st.text_input("Cosa vuoi chiedere?")
+
+    if user_question:
+        with st.spinner("Sto pensando..."):
+            response = st.session_state.conversation_chain.invoke(
+                {"question": user_question}
+            )
+            st.write(response["answer"])
 
 
 if __name__ == "__main__":
